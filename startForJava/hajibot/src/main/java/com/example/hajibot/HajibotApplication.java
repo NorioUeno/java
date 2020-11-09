6e889f49372a08816bd8ac9e0c036d346195ba0b
NORIO_UENO <Norio.Ueno@ibm.com>
2020-11-10 00:13:00 +0900

    revise .gitignore

---
+++ startForJava/hajibot/src/main/java/com/example/hajibot/HajibotApplication.java
@@ -0,0 +1,13 @@
+package com.example.hajibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibotApplication.class, args);
+	}
+
+}
